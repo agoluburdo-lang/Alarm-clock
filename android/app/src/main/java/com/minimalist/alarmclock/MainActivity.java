8ae52e4a2fc97da080768751ba62351f2dcc3fb5
agoluburdo-lang <a.goluburdo@gmail.com>
2026-06-24 23:32:04 +0200

    build: fix gradlew execution and enable autoplay
    Correct line endings for Gradle wrapper in CI and configure WebView to allow media playback without user gestures for alarm reliability.

--- android/app/src/main/java/com/minimalist/alarmclock/MainActivity.java
+++ android/app/src/main/java/com/minimalist/alarmclock/MainActivity.java
@@ -1,5 +1,24 @@
 package com.minimalist.alarmclock;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Отключаем обязательное требование кликов по экрану для воспроизведения звуков будильника
+        WebView webView = this.getBridge().getWebView();
+        if (webView != null) {
+            WebSettings settings = webView.getSettings();
+            settings.setMediaPlaybackRequiresUserGesture(false);
+        }
+    }
+}
